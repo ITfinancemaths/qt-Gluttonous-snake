ef88c7ac5f2cfaece543c7e97b0ac21b27eef8c9
ITfinancemaths <109284596+ITfinancemaths@users.noreply.github.com>
2023-12-21 18:01:11 +0800

    Add files via upload

---
+++ snake/widget.h
@@ -0,0 +1,55 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QKeyEvent>
+#include <QRectF>
+#include <QPainter>
+#include <QPen>
+#include <QBrush>
+#include <QDebug>
+#include <QTimer>
+#include <QTime>
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+protected:
+    void paintEvent(QPaintEvent *);
+    void keyPressEvent(QKeyEvent *);
+
+private:
+    void addTopRectF();
+    void addDownRectF();
+    void addLeftRectF();
+    void addRightRectF();
+    void deleteLastRectF();
+    bool snakeStrike();
+    enum Move{Left,Right,Up,Down};
+protected slots:
+    void timeOut();
+    void rewardTimeOut();
+
+private:
+    Ui::Widget *ui;
+    QList<QRectF> snake;//贪吃蛇本体
+    int snakeNodeWidth = 10;
+    int snakeNodeHeight = 10;
+    QTimer *timer;
+    QTimer *rewardTimer;
+    int time = 100;
+    int moveFlage = Up;
+    bool gameOver = false;
+    bool gameStart = false;
+    QList<QRectF> rewardNode;//奖励节点
+
+};
+
+#endif // WIDGET_H
